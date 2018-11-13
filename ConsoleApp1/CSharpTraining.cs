2b71a9b3a6000d98ac7eef45c9d0f26a854f891f
Katharine Theresa Ferguson <43380453+TreeGitHub@users.noreply.github.com>
2018-11-13 17:28:36 +0200

    More training exercises

--- ConsoleApp1/CSharpTraining.cs
+++ ConsoleApp1/CSharpTraining.cs
@@ -15,10 +15,137 @@ namespace ConsoleApp1
             //MultiplyArrays();
             //RemoveLetters();
             //LowestLargest();
-            NearestToTwenty();
-           
+            //NearestToTwenty();
+            //Matrix();  
+            //ReverseLetters();
+            //CheckThirty();
+            //AddIfString();
+            //RemoveCharacter();
+            SwapFirstLast();
         }
 
+        private static void SwapFirstLast()
+        {
+            string sString = "Theresa";
+            
+            Console.WriteLine(sString.Substring(sString.Length - 1) + sString.Substring(1, sString.Length - 2) + sString.Substring(0, 1));
+            Console.Read();
+        }
+        private static void RemoveCharacter()
+        {
+            string sString1 = "Theresa";
+            int iLength = 7;
+
+            Console.WriteLine(sString1);
+            for (int iCounter = 0; iCounter < iLength; iCounter++)
+            {
+                Console.WriteLine(sString1.Remove(iCounter, 1));
+            }
+            Console.Read();
+        }
+        private static void AddIfString()
+        {
+            string sString1 = "else";
+            string sString2 = "If not";
+            string sAddIf = "If ";
+          
+            Console.WriteLine((sString1.Substring(0, 3).Equals(sAddIf) ? sString1 : sString1.Insert(0, sAddIf)));
+            Console.WriteLine((sString2.Substring(0, 3).Equals(sAddIf) ? sString2 : sString2.Insert(0, sAddIf)));
+            Console.Read();
+        }
+        private static void CheckThirty()
+        {
+            int iNumber1 = 30;
+            int iNumber2 = 20;
+            bool bThirty = false;
+
+            bThirty = iNumber1 == 30 || iNumber2 == 30 || (iNumber1 + iNumber2 == 30);
+            Console.WriteLine(bThirty);
+
+            iNumber1 = 20;
+            iNumber2 = 10;
+            bThirty = false;
+
+            bThirty = iNumber1 == 30 || iNumber2 == 30 || (iNumber1 + iNumber2 == 30);
+            Console.WriteLine(bThirty);
+
+            iNumber1 = 10;
+            iNumber2 = 10;
+            bThirty = false;
+
+            bThirty = iNumber1 == 30 || iNumber2 == 30 || (iNumber1 + iNumber2 == 30);
+            Console.WriteLine(bThirty);
+
+            Console.Read();
+        }
+        private static void ReverseLetters()
+        {
+            char[] cCharacters;
+            int iCharacters;
+
+            Console.Write("How many letters would you like to enter?:");
+            iCharacters = Convert.ToInt32(Console.ReadLine());
+            cCharacters = new char[iCharacters];
+
+            for (int iCounter =0;iCounter < iCharacters; iCounter++)
+            {
+                Console.Write("Enter character {0}: ", iCounter);
+                cCharacters[iCounter] = Convert.ToChar( Console.ReadLine());
+            }
+            Console.Write("You entered: "); 
+            for (int iCounter = 0; iCounter < iCharacters; iCounter++)
+            {
+                Console.Write(cCharacters[iCounter]);
+            }
+            Console.WriteLine();
+            Console.Write("The reverse order is: ");
+            for (int iCounter = iCharacters-1; iCounter > -1; iCounter--)
+            {
+                Console.Write(cCharacters[iCounter]);
+            }
+            
+            Console.Read();
+        }
+
+        private static void Matrix()
+        {
+            int[][] myMatrix;
+
+            myMatrix = new int[][]
+            {
+                new int[]{0,2,3,2},
+                new int[]{0,6,0,1},
+                new int[]{1,2,3,4}
+            };
+            DisplayMatrix(myMatrix);
+            Console.WriteLine();
+            Console.WriteLine(SumMatrix(myMatrix));
+            Console.Read();
+        }
+        private static void DisplayMatrix(int[][] myMatrix)
+        {
+            for (int j = 0; j < myMatrix.Length; j++)
+            {
+                if (j > 0) { Console.WriteLine(); }
+                for (int i = 0; i < myMatrix[0].Length; i++)
+                    if (myMatrix[j][i] == 0)
+                    {
+                        Console.Write("X ");
+                    }
+                    else
+                    {
+                        Console.Write(myMatrix[j][i] + " ");
+                    }
+            }
+        }
+        private static int SumMatrix(int[][] myMatrix)
+        {
+            int x = 0;
+            for (int j = 0; j < myMatrix.Length; j++)
+                for (int i = 0; i < myMatrix[0].Length; i++)
+                    if (myMatrix[j][i] > 0) { x += myMatrix[j][i]; }
+            return x;
+        }
         private static void NearestToTwenty()
         {
             Console.Write("Enter first number: ");
